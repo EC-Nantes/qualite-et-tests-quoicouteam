c0d7dec88de095d2c9d2b394bfde89800110059a
mattlerigolo408 <144464428+mattlerigolo408@users.noreply.github.com>
2023-12-08 15:38:10 +0100

    Update TicTacToe.py by adding the function game

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,5 +7,24 @@ def check_winner(plateau) :
     for position in winning_positions :
         if plateau[position[0]] == plateau[position[1]] == plateau[position[2]] != 0:
             print(f"Player{plateau[position[0]]} wins !")
-            return
+            return(True)
+    return(False)
+        
+def game() :
+    game_over = False
+    plateau = [0]*9
+    i = 0
+    while not game_over and i < 9 :
+        for row in [plateau[j*3:(j+1)*3] for j in range(3)]:
+            print("|".join(map(str, row)))
+            print("-"*9)
+        position = input(f"Player {i%2+1}, enter the position (1-9): ")
+        position = int(position)
+        while plateau[position-1] != 0 :
+            position = input("Position invalide, enter new position (1-9) :")
+            position = int(position)
+        position = int(position)
+        plateau[position-1] = i%2+1
+        game_over = check_winner(plateau)
+        i = i+1 
         
